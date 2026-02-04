5326afab9e2ee0782036e0d62d4596d6b7064afd
arif1101 <arifurrahmanarif223@gmail.com>
2026-02-04 18:31:33 +0600

    feat(auth): customize login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -137,14 +137,14 @@ const LoginPage: React.FC = () => {
 
   return (
     <div
-      className="min-h-screen flex items-center justify-center px-4 py-8"
-      style={{
-        backgroundImage: 'url(/images/BG_Frame.png)',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        backgroundColor: '#0D1117',
-      }}
+      className="min-h-screen flex items-center justify-center px-4 py-8 bg-gradient-to-r from-[#F5F3FF] via-[#EFF6FF] to-[#F0FDFA]"
+      // style={{
+      //   backgroundImage: 'url(/images/BG_Frame.png)',
+      //   backgroundSize: 'cover',
+      //   backgroundPosition: 'center',
+      //   backgroundRepeat: 'no-repeat',
+      //   backgroundColor: '#0D1117',
+      // }}
     >
       <div className="w-[669px]">
         {/* Logo */}
@@ -152,7 +152,7 @@ const LoginPage: React.FC = () => {
           <div className="flex items-center justify-center gap-3 mb-2">
             {/* <Image src={Logo} alt="Polymath" width={200} height={50} /> */}
 
-            <h1 className='text-white text-4xl font-bold'>InterviewFIo</h1>
+            <h1 className='text-[#7D68BC] text-4xl font-bold'>InterviewFIo</h1>
 
           </div>
         </div>
@@ -166,12 +166,12 @@ const LoginPage: React.FC = () => {
                 onSubmit={handleSubmit}
                 className="bg-white/4 rounded-xl p-8 shadow-2xl border-[1px] border-white/4 "
               >
-                <h2 className="text-[#00FFFF] text-[24px] font-semibold text-center mb-6">Admin Login</h2>
+                <h2 className="text-[#111827] text-[24px] font-semibold text-center mb-6">Admin Login</h2>
                 <div className="mb-5 bg-white/8  h-[2px]" />
                 <div className="space-y-6">
                   {/* Email Field */}
                   <div>
-                    <label htmlFor="email" className="block text-[#FFFFFF] text-[18px] font-normal mb-2">
+                    <label htmlFor="email" className="block text-[#111827] text-[18px] font-normal mb-2">
                       Enter mail
                     </label>
                     <input
@@ -181,15 +181,15 @@ const LoginPage: React.FC = () => {
                       value={formData.email}
                       onChange={handleInputChange}
                       placeholder="yourmail@mailhere"
-                      className={`w-full h-[56px] px-4 py-3 bg-[#0D1117] text-[#FFFFFF] rounded-lg border ${errors.email ? 'border-red-500' : 'border-gray-600'
-                        } focus:outline-none focus:ring-2 focus:ring-[#00FFFF] focus:border-transparent placeholder-gray-400 transition-colors`}
+                      className={`w-full h-[56px] px-4 py-3 text-[#111827] rounded-lg border ${errors.email ? 'border-red-500' : 'border-[#E4DBFD]'
+                        } focus:outline-none focus:ring-2 focus:ring-[#E4DBFD] focus:border-transparent placeholder-[#967DE1]transition-colors`}
                     />
                     {errors.email && <p className="text-red-500 text-sm mt-1">{errors.email}</p>}
                   </div>
 
                   {/* Password Field */}
                   <div>
-                    <label htmlFor="password" className="block text-[#FFFFFF] text-[18px] font-normal mb-2">
+                    <label htmlFor="password" className="block text-[#111827] text-[18px] font-normal mb-2">
                       Enter Password
                     </label>
                     <div className="relative">
@@ -200,13 +200,13 @@ const LoginPage: React.FC = () => {
                         value={formData.password}
                         onChange={handleInputChange}
                         placeholder="••••••••••••••"
-                        className={`w-full h-[56px] px-4 py-3 pr-12 bg-[#0D1117] text-[#FFFFFF] rounded-lg border ${errors.password ? 'border-red-500' : 'border-gray-600'
-                          } focus:outline-none focus:ring-2 focus:ring-[#00FFFF] focus:border-transparent placeholder-gray-400 transition-colors`}
+                        className={`w-full h-[56px] px-4 py-3 pr-12 text-[#111827] rounded-lg border ${errors.password ? 'border-red-500' : 'border-[#E4DBFD]'
+                          } focus:outline-none focus:ring-2 focus:ring-[#E4DBFD] focus:border-transparent placeholder-[#967DE1] transition-colors`}
                       />
                       <button
                         type="button"
                         onClick={togglePasswordVisibility}
-                        className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400 hover:text-[#00FFFF] transition-colors"
+                        className="absolute right-3 top-1/2 transform -translate-y-1/2 text-[#967DE1] hover:text-[#967DE1] transition-colors"
                       >
                         {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
                       </button>
@@ -218,7 +218,7 @@ const LoginPage: React.FC = () => {
                   <button
                     type="submit"
                     disabled={isLoading}
-                    className="w-full h-[55px] py-3 bg-[#00FF7F] text-lg  text-[#0D1117] font-semibold rounded-lg hover:bg-[#00E66B] focus:outline-none focus:ring-2 focus:ring-[#00FF7F] focus:ring-offset-2 focus:ring-offset-[#1a2332] transition-colors disabled:opacity-50 disabled:cursor-not-allowed"
+                    className="w-full h-[55px] py-3 text-lg bg-gradient-to-r from-[#A78BFA] to-[#5835C0] text-white font-semibold rounded-lg focus:outline-none focus:ring-2 focus:ring-[#00FF7F] focus:ring-offset-2 focus:ring-offset-[#1a2332] transition-colors disabled:opacity-50 disabled:cursor-not-allowed"
                   >
                     {isLoading ? (
                       <div className="flex items-center justify-center gap-2">
@@ -232,13 +232,13 @@ const LoginPage: React.FC = () => {
 
                   {/* Remember Me & Forgot Password */}
                   <div className="flex items-center justify-between">
-                    <label className="flex items-center gap-2 text-[#FFFFFF] text-[18px]  font-normal cursor-pointer">
+                    <label className="flex items-center gap-2 text-[#111827] text-[18px]  font-normal cursor-pointer">
                       <input
                         type="checkbox"
                         name="rememberMe"
                         checked={formData.rememberMe}
                         onChange={handleInputChange}
-                        className="w-4 h-4 text-[#00FFFF] bg-[#0D1117] border-gray-600 rounded focus:ring-[#00FFFF] focus:ring-2"
+                        className="w-4 h-4 accent-[#967DE1] bg-[#0D1117] border-gray-600 rounded cursor-pointer"
                       />
                       Remember me
                     </label>
